2fdf2ed93a2fa83c285f2816bc43e9c8fef3c968
chenqiaochu-create <chenqiaochu@sjtu.edu.cn>
2022-05-14 22:33:49 +0800

    modify R code.

---
+++ protqc/R/qc_pipelines.R
@@ -0,0 +1,190 @@
+#' Linear normalization: 1~10
+#' @param x A value to be normalized
+#' @param x_max Queried maximum in historical values
+#' @param x_min Queried minimum in historical values
+#' @export
+qc_linear_norm <- function(x, x_min, x_max, decreasing=F) {
+  if (length(x) == 1) {
+    if (x <= x_min) {
+      x <- x_min
+    } else if (x >= x_max) {
+      x <- x_max
+    }
+  }
+  if (decreasing == F) {
+    x_norm <- sapply(x, function(a) 1 + (a - x_min) * 9 / (x_max - x_min))
+  } else {
+    x_norm <- sapply(x, function(a) 10 - (a - x_min) * 9 / (x_max - x_min))
+  }
+
+  return(x_norm <- round(x_norm, digits = 3))
+
+}
+
+#' Label the performance: bad, fair, good, great
+#' @param x The value of the metric to be labeled
+#' @param x_ref The reference values of the metric
+#' @param cutoff The cut-offs of the labeling
+#' @importFrom dplyr between
+#' @export
+
+qc_performance <- function(x, x_ref, cutoff=c(0, 0.2, 0.5, 0.8, 1)) {
+  ref_perc <- quantile(x_ref, cutoff, na.rm = T)
+  if (between(x, ref_perc[1], ref_perc[2])) {
+    x_class <- "Bad"
+  } else if (between(x, ref_perc[2], ref_perc[3])) {
+    x_class <- "Fair"
+  } else if (between(x, ref_perc[3], ref_perc[4])) {
+    x_class <- "Good"
+  } else if (between(x, ref_perc[4], ref_perc[5])) {
+    x_class <- "Great"
+  }
+
+  return(x_class)
+
+}
+
+#' Rank in historical performances
+#' @param x The value of the metric to be labeled
+#' @param x_ref The reference values of the metric
+#' @param cutoff The cut-offs of the labeling
+#' @export
+qc_rank <- function(x, x_hist) {
+  x_all <- c(x, x_hist[!is.na(x_hist)])
+  x_pos <- floor(rank(- x_all)[1])
+  x_rank <- c(paste(x_pos, "/", length(x_all), sep = ""))
+
+  return(x_rank)
+
+}
+
+#' Calculating all QC metrics
+#' @param pro_dt A expression table file (at protein level)
+#' @param pep_dt A expression table file (at peptide level)
+#' @param meta_dt A metadata file
+#' @param output_dir A directory for results
+#' @export
+
+qc_allmetrics <- function(pro_dt, meta_dt, pep_dt=NULL, output_dir=NULL) {
+  # Basic information ---------------------------
+  pro_info <- qc_info(pro_dt, meta_dt)
+
+  # SNR -----------------------------------------
+  snr_results <- qc_snr(pro_dt, meta_dt, output_dir)
+  snr_value <- snr_results$SNR
+
+  # RC ------------------------------------------
+  if (!is.null(pep_dt)) {
+    cor_results <- qc_cor(pep_dt, meta_dt, output_dir)
+    cor_value <- cor_results$COR
+  }else {
+    cor_results <- NULL
+    cor_value <- NA
+  }
+
+  # QC results ----------------------------------
+  metrics <- c(
+    "Number of features",
+    "Missing percentage (%)",
+    "Absolute Correlation",
+    "Coefficient of variantion (CV, %)",
+    "Signal-to-Noise Ratio (SNR)",
+    "Relative Correlation with Reference Datasets (RC)"
+  )
+  qc_values <- c(pro_info, snr_value, cor_value)
+
+  # Output --------------------------------------
+  output_table <- data.table(
+    "Quality Metrics" = metrics,
+    "Value" = c(pro_info, snr_value, cor_value)
+  )
+
+  all_results <- list(
+    snr_results = snr_results,
+    cor_results = cor_results,
+    output_table = output_table
+  )
+  return(all_results)
+}
+
+
+#' Calculating: Total Score
+#' @param allmetrics_dt the output table from qc_allmetrics
+#' @param normalized if True, the qc values will be linearly normalized to 1~10.
+#' @export
+
+qc_total <- function(allmetrics_dt,
+                     ref_qc, ref_qc_norm, ref_qc_stat, normalized=T) {
+  # Normalize & Rank: All metrics ----------------------
+  output_class <- c()
+  output_norm <- c()
+  output_rank <- c()
+  metrics <- allmetrics_dt$`Quality Metrics`
+  for (m in metrics) {
+    x <- allmetrics_dt$Value[allmetrics_dt$`Quality Metrics` %in% m]
+    x_ref_norm <- as.numeric(ref_qc_norm[, colnames(ref_qc_norm) %in% m])
+    x_ref <- ref_qc[, colnames(ref_qc) %in% m]
+
+    if (!is.na(x)) {
+      x_max <- max(x_ref, na.rm = T)
+      x_min <- min(x_ref, na.rm = T)
+      if (m %in% c("Coefficient of variantion (CV, %)",
+                   "Missing percentage (%)")) {
+        x_norm <- qc_linear_norm(x, x_min, x_max, decreasing = T)
+      } else {
+        x_norm <- qc_linear_norm(x, x_min, x_max)
+      }
+      x_rank <- qc_rank(x_norm, x_ref_norm)
+      x_class <- qc_performance(x_norm, x_ref_norm)
+
+    } else {
+      x_norm <- NA
+      x_rank <- NA
+      x_class <- NA
+    }
+
+    output_norm <- c(output_norm, x_norm)
+    output_rank <- c(output_rank, x_rank)
+    output_class <- c(output_class, x_class)
+  }
+
+  # Normalize & Rank: Total score ----------------------
+  total_ref <- as.numeric(ref_qc_norm$Total)
+  total_value <- round(geometric.mean(as.numeric(output_norm)), 3)
+  total_norm <- qc_linear_norm(total_value, min(total_ref), max(total_ref))
+
+  total_ref_norm <- as.numeric(ref_qc_norm$Total_norm)
+  total_rank <- qc_rank(total_norm, total_ref_norm)
+  total_c <- qc_performance(total_norm, total_ref_norm)
+
+  # Output ---------------------------------------------
+  allmetrics <- c(metrics, "Total", "Total_norm")
+  allnorm_dt <- data.table(
+    "Quality Metrics" = allmetrics,
+    "Value" = c(output_norm, total_value, total_norm)
+  )
+  allmetrics_dt <- rbind(
+    allmetrics_dt,
+    data.table(
+      "Quality Metrics" = "Total Score",
+      "Value" = total_norm
+    )
+  )
+  output_table <- merge(
+    allmetrics_dt,
+    ref_qc_stat,
+    by = "Quality Metrics"
+  )
+  output_table <- cbind(
+    output_table[c(4, 3, 1, 2, 6, 5, 7), ],
+    data.table(
+      "Rank" = c(output_rank, total_rank),
+      "Performance" = c(output_class, total_c)
+    )
+  )
+  output_list <- list(
+    Normalized = allnorm_dt,
+    Raw = output_table
+  )
+  return(output_list)
+}
--- protqc/README.md
+++ protqc/README.md
@@ -1,7 +1,7 @@
 # Packgage: protqc
 
 ## Description
-  The package protqc visualizes Quality Control(QC) results of proteomics data for Quartet Project. The QC pipeline starts from the expression profiles at peptide/protein levels, and enables to calculate 6 metrics. A Total score is the geometric mean of the linearly normalized values of these metrics.<br />
+  The package protqc output Quality Control(QC) results of proteomics data for Quartet Project. The QC pipeline starts from the expression profiles at peptide/protein levels, and enables to calculate 6 metrics. A Total score is the geometric mean of the linearly normalized values of these metrics.<br />
   1.	***Number of features***: We expect as many proteins (mapped to gene symbols) as possible for downstreaming analyses.
   2.	***Missing percentage (%)***: Too many missing values interfere with comparability. This metric is the median of missing percentages among samples.
   3.	***Coefficient of variantion (CV, %)***: A CV value is calculated to indicate the dispersion within replicates feature by feature.
@@ -21,40 +21,40 @@ devtools::install_github("chinese-quartet/quartet-protqc-report/protqc")
 ```
 
 ## Usage
-  protqc::table_conclusion(exp_path, meta_path, output_dir)
+  protqc::qc_conclusion(exp_path, meta_path, output_dir)
   > You can also use other QC functions (start with 'qc') to get performance for your data in each metric.
 
 ## Examples
 ```
-exp_path <- './data/example_data_for_test1.csv'
-meta_path <- './data/example_meta_for_test1.csv'
-output_dir <- './data/output'
-protqc::table_conclusion(exp_path, meta_path, output_dir)
+exp_path <- './test/input/example_data_for_test1.csv'
+meta_path <- './test/input/example_meta_for_test1.csv'
+output_dir <- './test/output/test1'
+protqc::qc_conclusion(exp_path, meta_path, output_dir)
 ```
 
 ## Built-in Data
-1. reference_dataset.rds<br />
+1. ./data/reference_dataset.rds<br />
    This file contains benchmarked historical data with relative quantities at peptide levels. For shot-gun proteomics, quantitation at peptide levels is theoretically more reliable. We use relatively quantitative expression values, for each sequence of each sample pair (D5/D6, F7/D6, M8/D6), as the reference for assessment of quantitative accuracy. 
    > For details in buiding the reference dataset, please refer to ... 
   
-2. historical_data_genesymbols.rds<br />
+2. ./data/historical_data_genesymbols.rds<br />
    This file contains historical datasets (at protein levels, mapped to genes), which are used for calculating historical performances.
 
-3. historical_data_peptides.rds<br />
+3. ./data/historical_data_peptides.rds<br />
    This file contains historical datasets (at peptide levels, unmapped sequences), which are used for calculating historical performances in quantitative accuracy at relative levels.
 
-4. historical_meta.rds<br />
+4. ./data/historical_meta.rds<br />
    This file contains historical metadata.
 
-5. historical_qc.rds & historical_qc_norm.rds<br />
+5. ./data/historical_qc.rds & ./data/historical_qc_norm.rds<br />
    These files are used for ranking scores of the testing data set in historical values.
    > These files are all output of the function ***qc_history()***.
 
 ## Examples for the input data
-1. example_data_for_test1.csv & example_data_for_test2.csv<br />
-   This file is an example for profiled data at protein levels and peptide levels (for these functions: ***input_data()***, ***qc_info()***, ***qc_snr()***, ***qc_cor()***, ***table_conclusion()***), containing quantitative levels in each sample (replicate). The first two columns ("Type" and "Feature") are required to explain the feature type, which is either "Gene Symbol", or "Peptide Sequence") and the feature name. The missing values of data at protein levels are replaced by *NA*, those at peptide levels are replaced by *0*.
+1. ./test/input/example_data_for_test1.csv & ./test/input/example_data_for_test2.csv<br />
+   This file is an example for profiled data at protein levels and peptide levels (for these functions: ***input_data()***, ***qc_info()***, ***qc_snr()***, ***qc_cor()***, ***table_conclusion()***), containing quantitative levels in each sample (replicate). The first two columns ("Type" and "Feature") are required to explain the feature type, which is either "Gene Symbol", or "Peptide Sequence") and the feature name. The missing values of data are replaced by *NA* or *0*.
    > If you only provide data at protein levels (refer to the file *example_data_for_test2.csv*), then the metric RC will not be calculated.
 
-2. example_meta_for_test1.csv & example_meta_for_test1.csv<br />
-   This file is an example for proteomic metadata.
+2. ./test/input/example_meta_for_test1.csv & ./test/input/example_meta_for_test1.csv<br />
+   This file is an example for proteomic metadata. The columns "library" and "sample" are required.
    > The column names of profiled data (except the first two columns) and the column 'library' of metadata must be in one-to-one correspondence.
